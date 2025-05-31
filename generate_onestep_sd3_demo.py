9317e4770cba62fce37bf4181bc939d551dc6dfb
William Luo <32350533+pkulwj1994@users.noreply.github.com>
2025-05-31 22:15:38 +0800

    Add files via upload

---
+++ generate_onestep_sd3_demo.py
@@ -0,0 +1,233 @@
+# Copyright (c) 2025, Weijian Luo. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0
+# https://www.apache.org/licenses/LICENSE-2.0.txt
+
+import os
+import re
+import gc
+import copy
+import click
+import tqdm
+import pickle
+import numpy as np
+import torch
+import PIL.Image
+import dnnlib
+from torch_utils import distributed as dist
+from inference_helper_func import load_sd3, sd3_sampler
+
+
+class StackedRandomGenerator:
+    """Wrapper for torch.Generator supporting different seeds per minibatch sample"""
+    def __init__(self, device, seeds):
+        self.generators = [
+            torch.Generator(device).manual_seed(int(seed) % (1 << 32))
+            for seed in seeds
+        ]
+
+    def randn(self, size, **kwargs):
+        assert size[0] == len(self.generators)
+        return torch.stack([
+            torch.randn(size[1:], generator=gen, **kwargs)
+            for gen in self.generators
+        ])
+
+    def randn_like(self, input):
+        return self.randn(
+            input.shape, 
+            dtype=input.dtype,
+            layout=input.layout,
+            device=input.device
+        )
+
+
+def parse_int_list(s):
+    if isinstance(s, list): return s
+    ranges = []
+    range_re = re.compile(r'^(\d+)-(\d+)$')
+    for p in s.split(','):
+        m = range_re.match(p)
+        if m:
+            ranges.extend(range(int(m.group(1)), int(m.group(2))+1))
+        else:
+            ranges.append(int(p))
+    return ranges
+
+def read_prompts(filename):
+    """Read text prompts from file"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        return [line.strip() for line in f if line.strip()]
+
+
+def save_as_npz(folder_path, max_images=50000):
+    """Compress folder images to NPZ file"""
+    image_exts = ('.png', '.jpg', '.jpeg')
+    image_files = [
+        f for f in os.listdir(folder_path)
+        if f.lower().endswith(image_exts)
+    ][:max_images]
+
+    images = []
+    for filename in tqdm.tqdm(image_files, desc="Compressing images"):
+        img = PIL.Image.open(os.path.join(folder_path, filename))
+        images.append(np.array(img))
+
+    npz_path = f"{folder_path}.npz"
+    np.savez(npz_path, arr_0=np.stack(images))
+    print(f"Saved {len(images)} images to {npz_path}")
+
+
+@click.command()
+@click.option('--network', 'network_pkl', required=True,
+              help='Network pickle filename', metavar='PATH|URL')
+@click.option('--outdir', required=True,
+              help='Output directory', metavar='DIR')
+@click.option('--seeds', default='0-63', show_default=True,
+              help='Random seeds', metavar='LIST', type=parse_int_list)
+@click.option('--subdirs', is_flag=True,
+              help='Create subdirectories for every 1000 seeds')
+@click.option('--batch', 'max_batch_size', default=16, show_default=True,
+              help='Maximum batch size', type=click.IntRange(min=1))
+@click.option('--init_timestep', default=0, show_default=True,
+              help='Initial timestep (0-999)', type=click.IntRange(min=0))
+@click.option('--resolution', default=1024, show_default=True,
+              help='generation resolution', type=click.IntRange(min=0))
+@click.option('--out_resolution', default=1024, show_default=True,
+              help='save resolution', type=click.IntRange(min=0))
+@click.option('--text_prompts', default='prompts/captions.txt', show_default=True,
+              help='Prompt file', metavar='PATH')
+@click.option('--repo_id', default='runwayml/stable-diffusion-v1-5', show_default=True,
+              help='Diffusion pipeline ID', metavar='ID')
+@click.option('--use_fp16', default=True, show_default=True,
+              help='Enable mixed-precision', type=bool)
+@click.option('--enable_xformers', default=True, show_default=True,
+              help='Enable xformers', type=bool)
+@click.option('--custom_seed', is_flag=True,
+              help='Use custom seeds')
+@click.option('--compile', is_flag=True, help='Enable JIT compilation with torch.compile')
+def main(
+    network_pkl,
+    outdir,
+    seeds,
+    subdirs,
+    max_batch_size,
+    resolution,
+    out_resolution,
+    init_timestep,
+    text_prompts,
+    repo_id,
+    compile,
+    use_fp16=True,
+    enable_xformers=True, 
+    custom_seed=False,
+):
+    """Generate images from one-step models"""
+    dist.init()
+    device = torch.device('cuda')
+    dtype = torch.bfloat16 if use_fp16 else torch.float32
+    captions = read_prompts(text_prompts)
+
+    # Prepare seed batches
+    num_batches = (len(seeds) - 1) // (max_batch_size * dist.get_world_size()) + 1
+    all_batches = torch.as_tensor(seeds).tensor_split(num_batches)
+    rank_batches = all_batches[dist.get_rank()::dist.get_world_size()]
+
+
+    ## load SD3.5 model
+    G_ema, vae, noise_scheduler, text_encoder_one, text_encoder_two, text_encoder_three, tokenizer_one, tokenizer_two, tokenizer_three, image_processor = load_sd3(pretrained_model_name_or_path=repo_id, pretrained_vae_model_name_or_path=repo_id,
+                                                            device=device, weight_dtype=dtype, enable_xformers=enable_xformers,)
+    
+    sample_scheduler = copy.deepcopy(noise_scheduler)
+    gen_timestep = noise_scheduler.timesteps[init_timestep]
+
+    latent_img_channels = G_ema.config.in_channels
+    vae_scale_factor = 2 ** (len(vae.config.block_out_channels) - 1)
+    latent_resolution = resolution // vae_scale_factor
+
+    # Load models
+    if dist.get_rank() == 0:
+        dist.print0(f'Loading network from "{network_pkl}"...')
+    with dnnlib.util.open_url(network_pkl, verbose=(dist.get_rank() == 0)) as f:
+        G_ema = pickle.load(f)['ema'].to(device, dtype)
+        # G_ema.disable_xformers_memory_efficient_attention()
+        G_ema.eval().requires_grad_(False)
+
+    gc.collect(); torch.cuda.empty_cache()
+
+    # Create output directory
+    output_dir = outdir
+    dist.print0(f'Generating {len(seeds)} images to "{output_dir}"...')
+
+    # Generate images
+    for batch_seeds in tqdm.tqdm(rank_batches, unit='batch', disable=(dist.get_rank() != 0)):
+        if not batch_seeds.numel():
+            continue
+
+        # Prepare seeds and captions
+        if custom_seed:
+            actual_seeds = [seeds[i] for i in batch_seeds]
+        else:
+            actual_seeds = batch_seeds.tolist()
+            
+        rnd = StackedRandomGenerator(device, actual_seeds)
+        batch_captions = [captions[i] for i in batch_seeds]
+
+        # Generate latent vectors
+        latents = rnd.randn([len(batch_seeds), latent_img_channels, latent_resolution, latent_resolution], device=device, dtype=dtype)
+        gen_timesteps = gen_timestep * torch.ones((len(batch_captions),), device=device, dtype=torch.float32)
+
+        # Run generator
+        with torch.inference_mode():
+            images = sd3_sampler(
+                transformer=G_ema,
+                latents=latents,
+                contexts=batch_captions,
+                gen_timesteps=gen_timesteps,
+                noise_scheduler=sample_scheduler,
+                text_encoder_one=text_encoder_one, 
+                text_encoder_two=text_encoder_two, 
+                text_encoder_three=text_encoder_three, 
+                tokenizer_one=tokenizer_one, 
+                tokenizer_two=tokenizer_two, 
+                tokenizer_three=tokenizer_three, 
+                image_processor=image_processor,
+                resolution=None, ## not used
+                dtype=dtype, 
+                return_images=False, 
+                vae=None
+                )
+
+        images = (images / vae.config.scaling_factor) + vae.config.shift_factor
+        images = vae.decode(images, return_dict=False)[0]
+        images = image_processor.postprocess(images, output_type='pt')
+
+        images = (images * 255).clamp(0, 255).to(torch.uint8)
+        images_np = images.permute(0, 2, 3, 1).cpu().numpy()
+
+        for seed, image_np, caption in zip(batch_seeds, images_np, batch_captions):
+            subdir = os.path.join(output_dir, f'{seed//1000*1000:06d}') if subdirs else output_dir
+            os.makedirs(subdir, exist_ok=True)
+            
+            base_path = os.path.join(subdir, f'{seed:06d}')
+
+            PIL.Image.fromarray(image_np).resize((out_resolution, out_resolution)).save(f'{base_path}.png')
+            
+            with open(f'{base_path}.txt', 'w', encoding='utf-8') as f:  # ADDED ENCODING
+                f.write(caption)
+
+
+    dist.print0('Done.')
+
+
+if __name__ == "__main__":
+    main()
+
+    # torchrun --standalone --nproc_per_node=1 /cpfs/user/weijian/code/onestep_SD3.5/generate_onestep_sd3.py \
+    # --init_timestep 0 \
+    # --text_prompts '/cpfs/user/weijian/code/onestep_SD3.5/prompts/parti_prompts.txt' \
+    # --repo_id '/cpfs/user/weijian/data/download/stabilityai/stable-diffusion-3.5-medium' \
+    # --outdir='/cpfs/user/weijian/evaluate/parti/sd3_onestep' \
+    # --seeds=0-2000 \
+    # --batch=32 \
+    # --network='/cpfs/user/weijian/logs/onestep_sd3/E01_ct/00002-aesthetics-glr1e-05-lr1e-05-initsigma0-gpus16-batch512-tmax980-fp32-method_DI-*-rlhfFalse-rmPS-rscale100.0-cfg4.5-l2scale0.0/network-snapshot-1.000000-000614.pkl'
---
+++ inference_helper_func.py
@@ -0,0 +1,358 @@
+import torch
+import diffusers
+from transformers import AutoTokenizer, PretrainedConfig, CLIPTextModel
+from diffusers import (
+    AutoencoderKL,
+    FlowMatchEulerDiscreteScheduler,
+    SD3Transformer2DModel,
+)
+from packaging import version
+from diffusers.utils.import_utils import is_xformers_available
+
+
+# from diffusers.pipelines.stable_diffusion_3.pipeline_stable_diffusion_3 import retrieve_timesteps
+def upcast_vae(vae):
+    dtype = vae.dtype
+    vae.to(dtype=torch.float32)
+
+@torch.no_grad()
+def tokenize_prompt(tokenizer, prompt):
+    text_inputs = tokenizer(
+        prompt,
+        padding="max_length",
+        max_length=tokenizer.model_max_length,
+        truncation=True,
+        return_tensors="pt",
+    )
+    text_input_ids = text_inputs.input_ids
+    return text_input_ids
+
+
+def tokenize_captions(tokenizer, examples):
+    max_length = tokenizer.model_max_length
+    captions = []
+    for caption in examples:
+        captions.append(caption)
+
+    text_inputs = tokenizer(
+        captions, truncation=True, padding="max_length", max_length=max_length, return_tensors="pt"
+    )
+
+    return text_inputs.input_ids
+
+
+def _encode_prompt_with_t5(
+    text_encoder,
+    tokenizer,
+    max_sequence_length,
+    prompt=None,
+    text_input_ids=None,
+    num_images_per_prompt=1,
+    device=None,
+):
+    prompt = [prompt] if isinstance(prompt, str) else prompt
+    if prompt is not None:
+        batch_size = len(prompt)
+    else:
+        batch_size = text_input_ids.shape[0]
+
+    if tokenizer is not None:
+        text_inputs = tokenizer(
+            prompt,
+            padding="max_length",
+            max_length=max_sequence_length,
+            truncation=True,
+            add_special_tokens=True,
+            return_tensors="pt",
+        )
+        text_input_ids = text_inputs.input_ids
+    else:
+        if text_input_ids is None:
+            raise ValueError("text_input_ids must be provided when the tokenizer is not specified")
+    prompt_embeds = text_encoder(text_input_ids.to(device))[0]
+
+    dtype = text_encoder.dtype
+    prompt_embeds = prompt_embeds.to(dtype=dtype, device=device)
+
+    _, seq_len, _ = prompt_embeds.shape
+
+    # duplicate text embeddings and attention mask for each generation per prompt, using mps friendly method
+    prompt_embeds = prompt_embeds.repeat(1, num_images_per_prompt, 1)
+    prompt_embeds = prompt_embeds.view(batch_size * num_images_per_prompt, seq_len, -1)
+
+    return prompt_embeds
+
+def _encode_prompt_with_clip(
+    text_encoder,
+    tokenizer,
+    prompt: str,
+    device=None,
+    text_input_ids=None,
+    num_images_per_prompt: int = 1,
+):
+    prompt = [prompt] if isinstance(prompt, str) else prompt
+    if prompt is not None:
+        batch_size = len(prompt)
+    else:
+        batch_size = text_input_ids.shape[0]
+
+    if tokenizer is not None:
+        text_inputs = tokenizer(
+            prompt,
+            padding="max_length",
+            max_length=77,
+            truncation=True,
+            return_tensors="pt",
+        )
+
+        text_input_ids = text_inputs.input_ids
+    else:
+        if text_input_ids is None:
+            raise ValueError("text_input_ids must be provided when the tokenizer is not specified")
+
+    prompt_embeds = text_encoder(text_input_ids.to(device), output_hidden_states=True)
+
+    pooled_prompt_embeds = prompt_embeds[0]
+    prompt_embeds = prompt_embeds.hidden_states[-2]
+    prompt_embeds = prompt_embeds.to(dtype=text_encoder.dtype, device=device)
+
+    _, seq_len, _ = prompt_embeds.shape
+    # duplicate text embeddings for each generation per prompt, using mps friendly method
+    prompt_embeds = prompt_embeds.repeat(1, num_images_per_prompt, 1)
+    prompt_embeds = prompt_embeds.view(batch_size * num_images_per_prompt, seq_len, -1)
+
+    return prompt_embeds, pooled_prompt_embeds
+
+### FIXME need double check
+# Adapted from pipelines.StableDiffusion3Pipeline.encode_prompt
+@torch.no_grad()
+def encode_prompt(
+    text_encoders,
+    tokenizers,
+    prompt: str,
+    max_sequence_length=77,
+    device=None,
+    num_images_per_prompt: int = 1,
+    text_input_ids_list=None,
+):
+    prompt = [prompt] if isinstance(prompt, str) else prompt
+
+    if tokenizers is not None:
+        clip_tokenizers = tokenizers[:2]
+        t5_tokenizer = tokenizers[-1]
+    else:
+        clip_tokenizers = [None, None]
+        t5_tokenizer = None
+
+    clip_text_encoders = text_encoders[:2]
+
+    clip_prompt_embeds_list = []
+    clip_pooled_prompt_embeds_list = []
+    for i, (tokenizer, text_encoder) in enumerate(zip(clip_tokenizers, clip_text_encoders)):
+        prompt_embeds, pooled_prompt_embeds = _encode_prompt_with_clip(
+            text_encoder=text_encoder,
+            tokenizer=tokenizer,
+            prompt=prompt,
+            device=device if device is not None else text_encoder.device,
+            num_images_per_prompt=num_images_per_prompt,
+            text_input_ids=text_input_ids_list[i] if text_input_ids_list else None,
+        )
+        clip_prompt_embeds_list.append(prompt_embeds)
+        clip_pooled_prompt_embeds_list.append(pooled_prompt_embeds)
+
+    clip_prompt_embeds = torch.cat(clip_prompt_embeds_list, dim=-1)
+    pooled_prompt_embeds = torch.cat(clip_pooled_prompt_embeds_list, dim=-1)
+
+    t5_prompt_embed = _encode_prompt_with_t5(
+        text_encoder=text_encoders[-1],
+        tokenizer=t5_tokenizer,
+        max_sequence_length=max_sequence_length,
+        prompt=prompt,
+        num_images_per_prompt=num_images_per_prompt,
+        text_input_ids=text_input_ids_list[-1] if text_input_ids_list else None,
+        device=device if device is not None else text_encoders[-1].device,
+    )
+
+    clip_prompt_embeds = torch.nn.functional.pad(
+        clip_prompt_embeds, (0, t5_prompt_embed.shape[-1] - clip_prompt_embeds.shape[-1])
+    )
+    prompt_embeds = torch.cat([clip_prompt_embeds, t5_prompt_embed], dim=-2)
+
+    return prompt_embeds, pooled_prompt_embeds
+
+
+
+def import_model_class_from_model_name_or_path(
+    pretrained_model_name_or_path: str, revision: str, subfolder: str = "text_encoder"
+):
+    text_encoder_config = PretrainedConfig.from_pretrained(
+        pretrained_model_name_or_path, subfolder=subfolder, revision=revision
+    )
+    model_class = text_encoder_config.architectures[0]
+
+    if model_class == "CLIPTextModel":
+        from transformers import CLIPTextModel
+
+        return CLIPTextModel
+    elif model_class == "CLIPTextModelWithProjection":
+        from transformers import CLIPTextModelWithProjection
+
+        return CLIPTextModelWithProjection
+
+    elif model_class == "T5EncoderModel":
+        from transformers import T5EncoderModel
+
+        return T5EncoderModel
+    else:
+        raise ValueError(f"{model_class} is not supported.")
+
+
+def get_sigmas(noise_scheduler, timesteps, n_dim=4, dtype=torch.float32):
+    sigmas = noise_scheduler.sigmas.to(device=timesteps.device, dtype=dtype)
+    schedule_timesteps = noise_scheduler.timesteps.to(device=timesteps.device)
+    # print(f"schedule_timesteps: {schedule_timesteps}")
+    timesteps = timesteps.to(device=timesteps.device)
+    step_indices = [(schedule_timesteps == t).nonzero().item() for t in timesteps]
+
+    sigma = sigmas[step_indices].flatten()
+    while len(sigma.shape) < n_dim:
+        sigma = sigma.unsqueeze(-1)
+    return sigma
+
+def load_sd3(pretrained_model_name_or_path, pretrained_vae_model_name_or_path, device, weight_dtype, 
+              revision=None, variant=None, lora_config=None, enable_xformers=False, gradient_checkpointing=False):
+    # Load the tokenizer
+    print(f'pretrained_model_name_or_path: {pretrained_model_name_or_path}')
+    print(f'pretrained_vae_name_or_path: {pretrained_vae_model_name_or_path}')
+    print(f'revision: {revision}')
+
+    # Load the tokenizers
+    tokenizer_one = AutoTokenizer.from_pretrained(
+        pretrained_vae_model_name_or_path,
+        subfolder="tokenizer",
+        revision=revision,
+        use_fast=False,
+    )
+    tokenizer_two = AutoTokenizer.from_pretrained(
+        pretrained_vae_model_name_or_path,
+        subfolder="tokenizer_2",
+        revision=revision,
+        use_fast=False,
+    )
+    tokenizer_three = AutoTokenizer.from_pretrained(
+        pretrained_vae_model_name_or_path,
+        subfolder="tokenizer_3",
+        revision=revision,
+        use_fast=False,
+    )
+
+    # import correct text encoder classes
+    text_encoder_cls_one = import_model_class_from_model_name_or_path(
+        pretrained_vae_model_name_or_path, revision
+    )
+    text_encoder_cls_two = import_model_class_from_model_name_or_path(
+        pretrained_vae_model_name_or_path, revision, subfolder="text_encoder_2"
+    )
+    text_encoder_cls_three = import_model_class_from_model_name_or_path(
+        pretrained_vae_model_name_or_path, revision, subfolder="text_encoder_3"
+    )
+
+    # Load scheduler and models
+    noise_scheduler = FlowMatchEulerDiscreteScheduler.from_pretrained(pretrained_vae_model_name_or_path, subfolder="scheduler")
+    text_encoder_one = text_encoder_cls_one.from_pretrained(
+        pretrained_vae_model_name_or_path, subfolder="text_encoder", revision=revision, variant=variant
+    )
+
+    text_encoder_two = text_encoder_cls_two.from_pretrained(
+        pretrained_vae_model_name_or_path, subfolder="text_encoder_2", revision=revision, variant=variant
+    )
+
+    text_encoder_three = text_encoder_cls_three.from_pretrained(
+        pretrained_vae_model_name_or_path, subfolder="text_encoder_3", revision=revision, variant=variant
+    )
+
+    vae_path = (
+        pretrained_vae_model_name_or_path
+        if pretrained_vae_model_name_or_path is not None
+        else pretrained_model_name_or_path
+    )
+
+    vae = AutoencoderKL.from_pretrained(
+        vae_path,
+        subfolder="vae",
+        revision=revision,
+        variant=variant,
+    )
+
+    transformer = SD3Transformer2DModel.from_pretrained(pretrained_model_name_or_path, subfolder="transformer", revision=revision, variant=variant)
+
+    # Freeze untrained components
+    vae.requires_grad_(False)
+    text_encoder_one.requires_grad_(False)
+    text_encoder_two.requires_grad_(False)
+    text_encoder_three.requires_grad_(False)
+
+    # Move transformer and text_encoders to device and cast to weight_dtype
+    transformer.to(device, dtype=weight_dtype)
+    text_encoder_one.to(device, dtype=weight_dtype)
+    text_encoder_two.to(device, dtype=weight_dtype)
+    text_encoder_three.to(device, dtype=weight_dtype)
+    vae.to(device, dtype=weight_dtype)
+
+    from diffusers.image_processor import PipelineImageInput, VaeImageProcessor # adapted from: https://github.com/huggingface/diffusers/blob/main/src/diffusers/pipelines/stable_diffusion_3/pipeline_stable_diffusion_3.py#L219
+    vae_scale_factor = 2 ** (len(vae.config.block_out_channels) - 1)
+    image_processor = VaeImageProcessor(vae_scale_factor=vae_scale_factor)
+
+    if enable_xformers:
+        if is_xformers_available():
+            import xformers
+
+            xformers_version = version.parse(xformers.__version__)
+            if xformers_version == version.parse("0.0.16"):
+                ValueError(
+                    "xFormers 0.0.16 cannot be used for training in some GPUs. If you observe problems during training, please update xFormers to at least 0.0.17. See https://huggingface.co/docs/diffusers/main/en/optimization/xformers for more details."
+                )
+            transformer.enable_xformers_memory_efficient_attention()
+        else:
+            raise ValueError("xformers is not available. Make sure it is installed correctly")
+
+    if gradient_checkpointing:
+        transformer.enable_gradient_checkpointing()
+
+    return transformer, vae, noise_scheduler, text_encoder_one, text_encoder_two, text_encoder_three, tokenizer_one, tokenizer_two, tokenizer_three, image_processor
+
+
+def sd3_sampler(transformer, latents, contexts, gen_timesteps,  noise_scheduler, 
+                         text_encoder_one, text_encoder_two, text_encoder_three, tokenizer_one, 
+                         tokenizer_two, tokenizer_three, image_processor, resolution, dtype=torch.bfloat16,
+                         return_images=False, vae=None, guidance_scale=1):
+    
+    # Get the text embedding for conditioning
+    prompt=contexts
+    batch_size = len(prompt)
+
+    input_ids_one = tokenize_prompt(tokenizer_one, prompt)
+    input_ids_two = tokenize_prompt(tokenizer_two, prompt)    
+    input_ids_three = tokenize_prompt(tokenizer_three, prompt)
+
+    with torch.no_grad():
+        prompt_embeds, pooled_prompt_embeds = encode_prompt(
+            text_encoders=[text_encoder_one, text_encoder_two, text_encoder_three],
+            tokenizers=None,
+            prompt=None,
+            text_input_ids_list=[input_ids_one, input_ids_two, input_ids_three],
+        )
+
+    # from diffusers.pipelines.stable_diffusion_3.pipeline_stable_diffusion_3 import retrieve_timesteps
+    assert noise_scheduler.config.get("use_dynamic_shifting", None) == False # refer: https://github.com/huggingface/diffusers/blob/main/src/diffusers/pipelines/stable_diffusion_3/pipeline_stable_diffusion_3.py#L1009
+
+    x0 = torch.zeros_like(latents).to(latents.device)
+
+    latents = noise_scheduler.scale_noise(sample=x0, timestep=gen_timesteps, noise=latents) # mix 0 and noise to get a fake xt
+    noise_pred = transformer(hidden_states=latents, timestep=gen_timesteps, encoder_hidden_states=prompt_embeds, pooled_projections=pooled_prompt_embeds, return_dict=False)[0].to(dtype)
+    sigmas = get_sigmas(noise_scheduler, gen_timesteps, n_dim=latents.ndim, dtype=latents.dtype)
+
+    D_x = latents - sigmas* noise_pred
+
+    return D_x.to(dtype)
+
